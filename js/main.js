ffd88987bbf0840a865d0bd948836834e9fa85bf
Jinksi <ericjinks@gmail.com>
2015-10-03 15:34:05 +1000

    Typestuff

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 $(document).ready(function($){
   var pattern = Trianglify({
-  height: window.innerHeight,
-  width: window.innerWidth,
-  x_colors:['#444', '#1a1a1a'],
-  cell_size: 50
-});
+    height: window.innerHeight,
+    width: window.innerWidth,
+    x_colors:['#444', '#1a1a1a'],
+    cell_size: 50
+  });
 
   pattern.canvas(document.getElementById('bg2'));
 
+
+  function typeMe(){
+    var typeItems = [];
+    $('.info .typed span').each(function(){
+      typeItems.push($(this).text());
+    });
+    $(".info .typed").typed({
+      strings: typeItems,
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+      backDelay: 5000,
+      cursorChar: '<',
+      showCursor: false
+    });
+  }
+  // typeMe();
 });
